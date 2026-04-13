6aeb59fdae89d54564b7ab7a1bdbec0206644e12
jaime22232 <huillyjaime2024@gmail.com>
2026-04-12 19:07:43 -0500

    asd2

---
+++ for4.java
@@ -0,0 +1,11 @@
+// Mostrar los números pares del 1 al 20
+public class for4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <20; i++) {
+            if (i%2==0) {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
